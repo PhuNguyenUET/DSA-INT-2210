b44e7c1643eef9f678effe25753bc111e57cccca
Phu nguyen 22021138 <phunguyen.it.k67@gmail.com>
2023-09-05 17:17:04 +0700

    Week one - JavaIntro 1.8

---
+++ Week one/HiThree.java
@@ -0,0 +1,6 @@
+public class HiThree {
+    public static void main(String[] args) {
+        System.out.print("Hi, " + args[2] + ", " + args[1] + ", " + args[0]);
+        System.out.println(". How are you?");
+    }
+}
---
+++ Week one/Initials.java
@@ -0,0 +1,5 @@
+public class Initials {
+    public static void main(String[] args) {
+        System.out.println("I don't really see the point of doing this :v");
+    } 
+}
