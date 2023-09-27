abcc74906a6fcef2a5a12d7f017f5066e98b4c85
PhuNguyenUET <phunguyen.it.k67@gmail.com>
2023-09-27 07:25:42 +0700

    Week five update

--- Week_five/SelectionSort.java
+++ Week_five/SelectionSort.java
@@ -1,2 +1,21 @@
 public class SelectionSort {
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    public static void selectionSort (int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int mn = arr[i];
+            int mn_idx = i;
+            for (int j = i; j < n; j ++) {
+                if (arr[j] < mn) {
+                    mn = arr[j];
+                    mn_idx = j;
+                }
+            }
+            swap (arr, i, mn_idx);
+        }
+    }
 }
--- Week_two/ThreeSum_TwoPointer.java
+++ Week_two/ThreeSum_TwoPointer.java
@@ -1,2 +1,23 @@
+import java.util.List;
+
 public class ThreeSum_TwoPointer {
+    public void threeSumTwoPointer (int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i <= n - 3; i++) {
+            int j = i + 1;
+            for (int k = n - 1; k >= i + 1; k --) {
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
+                        System.out.println(nums[i]+ " " + nums[j] + " " + nums[k]);
+                        j ++;
+                    } else if (sum < 0) {
+                        j ++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
